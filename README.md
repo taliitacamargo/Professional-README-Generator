4e8639c2ffbbc66b257d3118e16ec3c92b3b0533
Tali <taliitacamargo@icloud.com>
2021-11-03 13:48:51 -0400

    new

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
-  ![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
+  ![License: Apache](https://img.shields.io/badge/License-Apache-blue.svg)
 
   # README Generator
 
 
   ## Description 
-  An application that generates README files contianing responses given by user
+  
 
 
   ## Content
@@ -22,13 +22,16 @@
   - [Test](#tests)
 
   ## Installation
-  npm i inquirer is used to install the application
+  An application that generates README files contianing responses given by user
   
   ## Usage 
-  It can save data that is input by the user into a README file
+  npm i inquirer is used to install the application
 
   ## License
-  ![License: MIT] (https://img.shields.io/badge/License-MIT-blue.svg
+  ![License: Apache] (https://img.shields.io/badge/License-Apache-blue.svg
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
 
   ## Contributions
   none
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -21,6 +21,48 @@ if (license != 'None') {
   
 }
 
+function renderLicenseSection(license) {
+  if (license.value = "MIT") {
+    return `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. `;
+
+}
+else if (license.value = "Apache") {
+return `Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`
+}
+else if 
+  (license.value = "GPL 3.0") {
+    return `This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  `
+  ;
+}
+  else {
+    return '';
+  }
+};
+
+
+  
+
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
   return `
@@ -54,6 +96,7 @@ function generateMarkdown(response) {
 
   ## License
   ${renderLicenseLink(response.license)}
+  ${renderLicenseSection(response.license)}
 
   ## Contributions
   ${response.contributions}
